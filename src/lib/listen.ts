adf85c70f4206e9afb2d2ba743c0dde760bede72
Lee Norris <lee.norris@arcadia.io>
2020-10-03 10:53:25 -0400

    chore: update listen

--- src/lib/listen.ts
+++ src/lib/listen.ts
@@ -8,8 +8,9 @@ export const listen = () => {
     process.stdin.on('data', d => stdin.next(d));
     process.stdin.on('close', () => stdin.complete());
 
-    process.stdout.on('data', d => stdout.next(d));
-    process.stdout.on('close', () => stdout.complete());
+    // process.stdout.on('data', d => stdout.next(d));
+    // process.stdout.on('close', () => stdout.complete());
+    stdout.asObservable().subscribe((d: string) => process.stdout.write(d));
 
     return { stdout, stdin };
 }
