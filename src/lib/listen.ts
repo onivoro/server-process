fb13e315f682bf688a62c85de6e2fd190c584f8e
Lee Norris <lee.norris@arcadia.io>
2020-10-03 10:28:58 -0400

    feat(listen): rx wrapper for stdout socket

--- src/lib/listen.ts
+++ src/lib/listen.ts
@@ -5,8 +5,11 @@ const stdout = new Subject();
 
 export const listen = () => {
 
-  process.stdin.on('data', d => stdin.next(d));
-  process.stdin.on('close', () => stdin.complete());
+    process.stdin.on('data', d => stdin.next(d));
+    process.stdin.on('close', () => stdin.complete());
 
-  return {stdout, stdin};
+    process.stdout.on('data', d => stdout.next(d));
+    process.stdout.on('close', () => stdout.complete());
+
+    return { stdout, stdin };
 }
